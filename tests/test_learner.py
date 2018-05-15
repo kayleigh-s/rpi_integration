f000197336590f662f40ceb2ba5150ee0418763d
JakeBrawer <jake.brawer@yale.edu>
2018-05-15 11:25:19 -0400

    [test/rpi_yale][WIP] Added more tests.

--- tests/test_learner.py
+++ tests/test_learner.py
@@ -44,6 +44,18 @@ class TestLearner(unittest.TestCase):
         self.curdir = os.path.dirname(os.path.abspath(__file__))
         self.maxDiff = None
 
+    def test_leg_foot(self):
+        print("Testing leg...")
+        self._compare_from_file("leg_foot.json")
+
+    def test_all_actions(self):
+        print("Testing all actions...")
+        self._compare_from_file("all_actions.json")
+
+    def test_full_chair(self):
+        print("Testing full chair...")
+        self._compare_from_file("full_chair.json")
+
     def test_One(self):
         self._delete()
         data = '[["u", "We will build a chair."], ["a", "get-screwdriver"]]'
@@ -73,6 +85,35 @@ class TestLearner(unittest.TestCase):
         url = self.addr + "/alpha/reset"
         requests.delete(url=url, headers=headers)
 
+    # Compares output of learning from input file to ground truth in output file
+    def _compare_from_file(self, f):
+        self._delete()
+
+        in_path = os.path.join(self.curdir, "in/", f)
+        out_path = os.path.join(self.curdir, "out/", f)
+
+        with open(in_path, "r") as i, open(out_path, "r") as o:
+            self._learn(i)
+
+            ground_truth = json.load(o)
+            output = json.loads(self._get().text)
+
+            self.assertEqual(ground_truth, output)
+
+    def _compare_from_string(self, f, *args):
+        self._delete()
+
+        out_path = os.path.join(self.curdir, "out/", f)
+
+        for a in args:
+            self._learn(a)
+
+        with open(out_path, "r") as o:
+            ground_truth = json.load(o)
+            output = json.loads(self._get().text)
+
+            self.assertEqual(ground_truth, output)
+
 
 # delete()
 # post(data)
@@ -82,11 +123,11 @@ class TestLearner(unittest.TestCase):
 # print(text)
 # print
 # gt = json.loads(text)
-# with open("./in/full_chair.json", "r", encoding='utf-8') as i:
-#     print("loading")
-#     data = json.load(i)
-#     print(data)
-#print(data == gt)
+delete()
+with open("./in/leg_foot.json", "r") as i:
+    print("loading")
+    post(i)
+    print(get().text)
 
 if __name__ == '__main__':
     unittest.main()
