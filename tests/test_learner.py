6c606b22898d4e0a7c5a6b0c210c138ebddd6780
JakeBrawer <jake.brawer@yale.edu>
2018-05-15 14:22:48 -0400

    [test/rpi_yale] Formatting.

--- tests/test_learner.py
+++ tests/test_learner.py
@@ -6,32 +6,6 @@ from io import open
 
 import requests
 
-addr = "http://0.0.0.0:5002"
-center_idxpost_addr = "{}/learn".format(addr)
-get_addr = "{}/alpha/gettree".format(addr)
-post_addr = "{}/learn".format(addr)
-headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-}
-
-
-def post(data):
-    req = requests.post(url=post_addr, data=data, headers=headers)
-    return req
-
-
-def get(frmt="json"):
-    assert (frmt == "json" or frmt == "pretty")
-    url = get_addr + "?format={}".format(frmt)
-    req = requests.get(url=url)
-    return req
-
-
-def delete():
-    url = addr + "/alpha/reset"
-    requests.delete(url=url, headers=headers)
-
 
 class TestLearner(unittest.TestCase):
     def setUp(self):
@@ -56,26 +30,12 @@ class TestLearner(unittest.TestCase):
         print("Testing full chair...")
         self._compare_from_file("full_chair.json")
 
-    def test_streaming(self):
+    def test_online(self):
         d1 = '[["u", "We will build a leg."], ["a", "get-dowel"]]'
         d2 = '[["a", "get-bracket-foot]]'
 
-        print("Testing streaming...")
+        print("Testing oline learning...")
         self._compare_from_string("leg_foot.json", d1, d2)
-
-    def test_One(self):
-        self._delete()
-        data = '[["u", "We will build a chair."], ["a", "get-screwdriver"]]'
-        self._learn(data)
-
-        with open(
-                os.path.join(self.curdir, "out/testOne.json"),
-                "r",
-                encoding="utf-8") as o:
-            ground_truth = json.load(o)
-            output = json.loads(self._get().text)
-            # print self._get().text
-            self.assertEqual(ground_truth, output)
 
     def _learn(self, data):
         url = self.addr + "/learn"
@@ -109,32 +69,21 @@ class TestLearner(unittest.TestCase):
 
     def _compare_from_string(self, f, *args):
         self._delete()
-
-        out_path = os.path.join(self.curdir, "out/", f)
-
         for a in args:
             self._learn(a)
+
+        out_path = os.path.join(self.curdir, "out/", f)
 
         with open(out_path, "r") as o:
             ground_truth = json.load(o)
             output = json.loads(self._get().text)
 
+            print(ground_truth)
+            print
+            print(self._get().text)
+
             self.assertEqual(ground_truth, output)
 
-
-# delete()
-# post(data)
-# g2 = get("json")
-# text = g2.text
-# text = re.sub("\"id\"\: \d+", "\"id\": ", text)
-# print(text)
-# print
-# gt = json.loads(text)
-# delete()
-# with open("./in/full_chair.json", "r") as i:
-#     print("loading")
-#     post(i)
-#     print(get().text)
 
 if __name__ == '__main__':
     unittest.main()
