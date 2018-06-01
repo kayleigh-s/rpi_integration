1ebe514ffcbd97e76f2d2c6b1e06a86e84431ddc
JakeBrawer <jake.brawer@yale.edu>
2018-06-01 15:24:55 -0400

    [planner] Added ability to change htm via query.

--- rpi_integration/planner.py
+++ rpi_integration/planner.py
@@ -1,26 +1,31 @@
 import argparse
-import threading
 import os
+import threading
 import time
-import rospy
 from random import shuffle
 
 from task_models.json_to_htm import json_to_htm
+from task_models.task import HierarchicalTask
 
+import rospy
 from human_robot_collaboration.controller import BaseController
 from human_robot_collaboration.service_request import finished_request
-
-from rpi_integration.learner_utils import parse_action
-
+from rpi_integration.learner_utils import RESTUtils, parse_action
+from std_msgs.msg import String
 
 parser = argparse.ArgumentParser("Run the autonomous HTM controller")
 parser.add_argument(
     '--path', help='path to the model files',
     default=os.path.join(os.path.abspath(os.path.dirname(__file__)),
                          "../tests/out/full_chair.json"))
+parser.add_argument(
+    '--do_query', help="If you want to query a subtask in a learned tree.",
+    type=bool, default=False)
 
 
-class HTMController(BaseController):
+
+
+class HTMController(BaseController, RESTUtils):
     """Controls Baxter using HTN derived from json"""
 
     strt_time = time.time()
@@ -28,6 +33,9 @@ class HTMController(BaseController):
     BRING = 'get_pass'
     HOLD_TOP = 'hold_top'
     HOLD_LEG ='hold_leg'
+
+    CHAIR_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              "../tests/out/full_chair.json")
 
     OBJECT_DICT = {
         "GET(seat)": (BRING, BaseController.LEFT, 198),
@@ -50,27 +58,42 @@ class HTMController(BaseController):
 
     }
 
-    def __init__(self, json_path):
+    def __init__(self, json_path, do_query):
+        self.json_path = json_path
         self.htm = json_to_htm(json_path)
         self.last_r = finished_request
-        super(HTMController, self).__init__(
+        BaseController.__init__(
+            self,
             left=True,
             right=True,
             speech=False,
             listen=True,
             recovery=True,
         )
+        RESTUtils.__init__(self)
+
+        self.do_query = do_query
+        self._learner_pub = rospy.Publisher('web_interface/json', String, queue_size=10)
+
+        # self.delete()
+        # self._train_learner_from_file()
+        rospy.loginfo('Ready!')
 
     @property
     def robot_actions(self):
         return self._get_actions(self.htm.root)
 
-    def _take_actions(self):
+    def _take_actions(self, actions):
         prev_arm = None # was left or right arm used previously?
         same_arm = True # Was previous action taken using the same arm as curr action?
         prev_same_arm = True # Was the previous previous action taken using same arm?
 
-        for a in self.robot_actions:
+        rospy.loginfo('Publishing first json')
+        self._learner_pub.publish(open(self.CHAIR_PATH, 'r').read())
+        rospy.sleep(5)
+        rospy.loginfo('Publishing second json')
+        self._learner_pub.publish(open(self.json_path, 'r').read())
+        for a in actions:
             cmd, arm, obj = parse_action(a, self.OBJECT_DICT)
             arm_str = "LEFT" if arm == 0 else 'RIGHT'
 
@@ -102,10 +125,31 @@ class HTMController(BaseController):
             prev_arm = arm
 
 
+    def _get_permuted_htm(self):
+        cmd1 = '[["u", "We will build the back first."]]'
+        cmd2 = '[["u", "We will build the screwdriver first."]]'
+
+        self._learner_pub.publish(self.get().text)
+        if self.listen_sub.wait_for_msg(timeout=20.):
+            rospy.loginfo("Received do_query command...")
+            self.query(cmd1)
+            rospy.loginfo("Query 1 result: {}".format(self.get().text))
+            self.query(cmd2)
+            rospy.loginfo("Query 2 result: {}".format(self.get().text))
+            self._learner_pub.publish(self.get().text)
+            htm = _learner_to_htm()
+            return htm
+
     def _run(self):
         rospy.loginfo('Starting autonomous control')
         rospy.sleep(3) # Add a little delay for self-filming!
-        self._take_actions()
+        if not self.do_query:
+            self._take_actions(self.robot_actions)
+        else:
+            rospy.loginfo("Running permuted htm")
+            htm = self._get_permuted_htm()
+            robot_actions = self._get_actions(htm.root)
+            self._take_actions(robot_actions)
 
     def _get_actions(self, root):
         """ Recursively retrieves actions in correct order """
@@ -126,11 +170,19 @@ class HTMController(BaseController):
             if root.action.agent == 'robot':
                 yield name
 
+    def _train_learner_from_file(self):
+        with open(self.CHAIR_PATH, 'r') as i:
+            self.learn(i)
+
+    def _learner_to_htm(self):
+        j = json.loads(get().text)
+        return HierarchicalTask(build_htm_recursively(j['nodes']))
+
 
 try:
     args = parser.parse_args()
     print(args.path)
-    controller = HTMController(args.path)
+    controller = HTMController(args.path, args.do_query)
 
     controller.run()
 except rospy.ROSInterruptException, KeyboardInterrupt:
