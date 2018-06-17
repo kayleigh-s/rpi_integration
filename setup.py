37dfc602aca006aa420ff46d95a2d0aa791889e0
JakeBrawer <jake.brawer@yale.edu>
2018-06-17 12:27:01 -0400

    Moved python module into src and planner into scripts.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['rpi_integration'],
+    package_dir={'': 'src'}
 )
 
 setup(**setup_args)
