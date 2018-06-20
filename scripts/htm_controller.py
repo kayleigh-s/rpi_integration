066998bf88b89f9b006a1fef669f78ee61e99255
JakeBrawer <jake.brawer@yale.edu>
2018-06-20 15:32:43 -0400

    Minor.

--- scripts/htm_controller.py
+++ scripts/htm_controller.py
@@ -4,7 +4,7 @@ from rpi_integration.htm_controoler import HTMController
 try:
     controller = HTMController()
 
-    if not rospy.get_param('/rpi_integretion/testing', False):
+    if not controller.testing:
         controller.run()
 
 except rospy.ROSInterruptException, KeyboardInterrupt:
