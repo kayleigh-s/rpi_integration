29f9f3b7b62d9da2a7dd49b728eab380ae7beb42
JakeBrawer <jake.brawer@yale.edu>
2019-02-21 15:24:57 -0500

    [WiP] Test files for ontosem controller.

--- tests/test_ontosem_controller.py
+++ tests/test_ontosem_controller.py
@@ -5,10 +5,14 @@ import requests
 
 from rpi_integration.ontosem_controller import OntoSemController
 
+PKG  = 'rpi_integration'
+NAME = 'test_ontosem_controller'
+
+
 class OntsemSim(object):
     def __init__(self):
         "Simulates HTTP messages from ontosem"
-    
+
         addr                          = "http://0.0.0.0:5002"
 
         self.GET_bootstrap_addr       = addr + "/yale/bootstrap"
@@ -31,6 +35,10 @@ class OntsemSim(object):
             req = requests.get(url=addr)
             return req
 
+        # Here I've simply inverted RESTOntoSemUtils methods
+        # from POSTs to GETS (and vice versa) so that we can send
+        # arbitrary messages to the controller
+
         def GET_bootstrap(self):
             return self._GET(self.GET_bootstrap_addr, id_dict)
 
@@ -46,7 +54,9 @@ class OntsemSim(object):
         def POST_robot_command(self, cmd_dict):
             return self._POST(self.POST_robot_command_addr, cmd_dict)
 
+# See https://wiki.ros.org/unittest for more info
 class TestOntoSemController(unittest.TestCase):
+
     def setUp(self):
         rospy.loginfo("Initializing test ontosem controller")
 
@@ -54,9 +64,31 @@ class TestOntoSemController(unittest.TestCase):
         self.ontosem    = OntsemSim()
 
     def test_bootstrap(self):
-        bootstap_dict = self.ontosem.GET_bootstrap()
-        rospy.loginfo("BOOT STRAP DICT: ", bootstap_dict)
+
+        gt_bootstrap_dict = {} # TODO: Fill this out
+        bootstap_dict     = self.ontosem.GET_bootstrap()
+
+        self.assertEqual(gt_bootstrap_doct, bootstap_dict)
+
+    def test_actions(self):
+        "Tests for correct callback after action"
+
+        cmd1 = {"get": "198", "callback":"SELF.CALLBACK.1"} # seat
+        cmd2 = {"get": "10", "callback":"SELF.CALLBACK.2"} # foot_1
+        cmds = [cmd1, cmd2]
+
+        cb1  = {"callback-id": "SELF.CALLBACK.1"}
+        cb2  = {"callback-id": "SELF.CALLBACK.2"}
+        cbs  = [cb1, cb2]
+
+        for cmd, cb in zip(cmds, cbs):
+            self.ontosem.POST_robot_command_addr(cmd) # Send command to controller
+            self.assertEqual(self.ontosem.GET_completed_action(), cb)
+
+    # TODO Implement (this one is important)
+    def test_perceptual_update(self):
+        raise NotImplementedError
 
 if __name__ == '__main__':
     import rostest
-    rostest.rosrun('rpi_integration', 'test_ontosem_controller', TestOntoSemController)
+    rostest.rosrun(PKG, NAME, TestOntoSemController)
