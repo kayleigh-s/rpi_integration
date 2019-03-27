2c224d69186bcb96b47b1206bcf4d3ee839c929b
JakeBrawer <jake.brawer@yale.edu>
2019-03-27 14:22:47 -0400

    Removed strange duplicate script.

--- scripts/ontomsem_controller.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import rospy
-from rpi_integration.ontosem_controller import OntoSemController
-
-rospy.loginfo("Running OntoSemController")
-controller = OntoSemController()
